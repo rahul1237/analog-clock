790016f2c8179f6868020e78b0546eb85235700b
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-02-11 03:45:23 +0530

    analog clock added

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// selecting all of the css classes on which we want to apply functionalities
 const hr = document.querySelector('.hr')
 const min = document.querySelector('.min')
 const sec = document.querySelector('.sec')
 
+// setting up the period of working
 setInterval(()=>{
+    // extracting the current time from DATE() function
     let day = new Date()
     let hour = day.getHours()
     let minutes = day.getMinutes()
     let seconds = day.getSeconds()
 
+    // formula that is explained above for the rotation of different hands
     let hrrotation = (30 * hour) + (0.5 * minutes)
     let minrotation = 6 * minutes
     let secrotation = 6 * seconds
